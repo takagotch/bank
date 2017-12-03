ce68e1768fa474b1a5779d8f421bb09d9b33519c
takagotch <dyaccb@gmail.com>
2017-12-03 20:54:53 +0900

    app/[controllers|layouts]/[store_controller|application].[rb|html.erb]

--- creditcard/app/controllers/store_controller.rb
+++ creditcard/app/controllers/store_controller.rb
@@ -1,17 +1,17 @@
 class StoreController < ApplicationController
   skip before_filter :authorize
   def index
-    if params[:set_locale]
-      redirect_to store_path(locale: params[:set_locale])
-    else
+#    if params[:set_locale]
+#      redirect_to store_path(locale: params[:set_locale])
+#    else
       @products = Product.latest
       @cart     = curent_cart
-    end
-
-    latest      = Product.latest
-    fresh_when etag: latest, last_modified: lastest.created_at.utc
-    expires_in 10minutes, public: true
-  end
+#    end
+#
+#    latest      = Product.latest
+#    fresh_when etag: latest, last_modified: lastest.created_at.utc
+#    expires_in 10minutes, public: true
+#  end
 end
 
 
